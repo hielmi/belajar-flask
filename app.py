80e1e16c88fa0bbd2bc06662b991deba1ea0f4be
Hielmi Sulaeman <hielmisulaeman10@gmail.com>
2023-10-21 12:15:42 +0700

    done integrasi-mysql

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
+#init main app
 app = Flask(__name__)
 
+#database config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'test'
 
+#init mysql
+mysql = MySQL(app)
+
+#set route
 @app.route("/")
 def index():
-    return "<h1> Hello, World! </h2>"
+
+    # cursor koneksi mysql
+    cursor = mysql.connection.cursor()
+
+    # eksekusi queri
+    cursor.execute("SELECT * FROM users")
+
+    #get data
+    data = cursor.fetchall()
+
+    #tutup kursor
+    cursor.close()
+
+    return render_template('index.html', users=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
