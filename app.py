3b189930c9d1df2b3b148876a6eebb233bb64eae
Hielmi Sulaeman <hielmisulaeman10@gmail.com>
2023-10-23 14:57:59 +0700

    change index to home

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 from flask_mysqldb import MySQL
 
 #init main app
@@ -13,32 +13,65 @@ app.config['MYSQL_DB'] = 'test'
 #init mysql
 mysql = MySQL(app)
 
+# Set the secret key
+app.secret_key = 'aplikasipertamaflask'
+
 #set route
-@app.route("/")
-def index():
-
-    # cursor koneksi mysql
-    cursor = mysql.connection.cursor()
-
-    # eksekusi queri
-    cursor.execute("SELECT * FROM users")
-
-    #get data
-    data = cursor.fetchall()
-
-    #tutup kursor
-    cursor.close()
-
-    return render_template('data.html', users=data)
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        email = request.form['username']
         password = request.form['password']
-        print(email)
+
+        if email == '' or password == '':
+            return redirect(url_for('login.html'))
+
+        cursor = mysql.connection.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (email, password))
+
+        result = cursor.fetchone()
+
+        if result:
+            session['is_logged_in'] = True
+            session['username'] = result[1]
+
+            print('password benar')
+            return redirect(url_for('home'))
+        else:
+            print('password salah')
+            return render_template('login.html')
     
     return render_template('login.html')
+
+@app.route("/home")
+def home():
+    if 'is_logged_in' in session:
+
+        # cursor koneksi mysql
+        cursor = mysql.connection.cursor()
+
+        # eksekusi queri
+        cursor.execute("SELECT * FROM users")
+
+        #get data
+        data = cursor.fetchall()
+
+        #tutup kursor
+        cursor.close()
+
+        return render_template('home.html', users=data)
+    
+    else: 
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('is_logged_in', None)
+    session.pop('username', None)
+
+    return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
